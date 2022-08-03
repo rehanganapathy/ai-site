79da942ef14632531fef3c6280a5bc3c56649f22
Rehan Ganapathy <79349712+rehanganapathy@users.noreply.github.com>
2022-08-04 00:46:47 +0530

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Model from "../components/model";
-import styles from "/Users/rehanganapathy/Desktop/ai-driven-app/frontend/ai-site/styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
